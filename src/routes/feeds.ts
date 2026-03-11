f8dde5589dd6332a0987ceac0714e86635bf1b16
Stephen Allen <720493+stetime@users.noreply.github.com>
2026-03-11 10:20:45 +0000

    add feeds post route

--- src/routes/feeds.ts
+++ src/routes/feeds.ts
@@ -1,9 +1,16 @@
 import { Hono } from "hono";
 import { z } from "zod";
-import { getFeedByUrl, getFeedsByUserId } from "@/db/queries/feeds";
-import { getSubscription } from "@/db/queries/subscriptions";
+import {
+	getFeedByUrl,
+	getFeedsByUserId,
+	insertFeed,
+	purgeFeed,
+} from "@/db/queries/feeds";
+import { insertPosts } from "@/db/queries/posts";
+import { addSubscription, getSubscription } from "@/db/queries/subscriptions";
 import { logger } from "@/lib/logger";
 import { requireAuth } from "@/lib/middleware";
+import { feedSanityCheck, fetchFeed, parse } from "@/lib/rss";
 import type { AppContext } from "@/lib/types";
 
 export const feeds = new Hono<AppContext>();
@@ -13,14 +20,13 @@ const postFeedSchema = z.object({
 
 feeds.get("/feeds", requireAuth, async (c) => {
 	const user = c.get("user");
-	if (!user) return c.json({ error: "unauthorized" }, 401);
-	const userFeeds = await getFeedsByUserId(user.id);
+	// biome-ignore lint/style/noNonNullAssertion: requireAuth confirms user exists
+	const userFeeds = await getFeedsByUserId(user!.id);
 	return c.json(userFeeds);
 });
 
 feeds.post("/feeds", requireAuth, async (c) => {
 	const user = c.get("user");
-	logger.debug("hitting the post");
 	if (!user) return c.json({ error: "unauthorized" }, 401);
 	const body: unknown = await c.req.json().catch(() => null);
 	if (!body) return c.json({ error: "invalid or missing request body" }, 400);
@@ -31,11 +37,43 @@ feeds.post("/feeds", requireAuth, async (c) => {
 	const { url } = validation.data;
 	const existingFeed = await getFeedByUrl(url);
 	if (existingFeed) {
+		logger.debug("we hit an existing feed.");
 		const existingSub = await getSubscription(user.id, existingFeed.id);
 		if (existingSub) {
+			logger.debug(`existing subscription found for this user and feed`);
 			return c.json({ error: "already subscribed" }, 409);
 		}
-		// create a sub, return code 201
+		await addSubscription(user.id, existingFeed.id);
+		return c.json(existingFeed, 201);
 	}
-	// it doesn't exist and we need to parse, insert and return it.
+	const remoteFeed = await fetchFeed(url);
+	if (!remoteFeed) return c.json({ error: "could not fetch feed" }, 400);
+	const { xml } = remoteFeed;
+	if (!feedSanityCheck(xml))
+		return c.json({ error: "not a valid rss/atom feed" }, 400);
+	const parsed = parse(xml);
+	logger.debug(
+		`parsed ${url} as ${parsed.title}, attempting to insert into the database`,
+	);
+	const insertedFeed = await insertFeed({
+		url,
+		title: parsed.title ?? (parsed.link as string),
+		description: parsed.description,
+		etag: remoteFeed.etag ?? undefined,
+		lastModified: remoteFeed.lastModified ?? undefined,
+	});
+	if (!insertedFeed) return c.json({ error: "failed to insert feed" }, 500);
+	await insertPosts(insertedFeed.id, parsed.entries ?? []);
+	await addSubscription(user.id, insertedFeed.id);
+	return c.json(insertedFeed, 201);
+});
+
+feeds.delete("/feeds/:id", requireAuth, async (c) => {
+	const user = c.get("user")
+	if (!user) return c.json({error: "not authorised"}, 403)
+	
+	// confirm the feed exists
+	// if it's a multi sub, just remove the current user's sub
+	// if there's only one user subbed, delete the whole thing
+	await purgeFeed(c.req.param("id"));
 });
